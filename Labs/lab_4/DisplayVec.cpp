1afa53ee00eb323ce928cfe45f0e3c3a9fc6e93d
CynicalNInja <146404386+CynicalNInja@users.noreply.github.com>
2023-11-06 21:54:06 -0800

    Update DisplayVec.cpp

--- Labs/lab_4/DisplayVec.cpp
+++ Labs/lab_4/DisplayVec.cpp
@@ -1,1 +1,41 @@
-//DisplayVec.cpp
+//DisplayVec.cpp 
+#include "DisplayVec.h" 
+
+/**
+ * @brief construct a new display vec :: display Vec object 
+ * 
+ */
+ DisplayVec:: DisplayVec(){
+
+ }
+
+ /**
+  * @brief prints the integers in vec   
+  * 
+  * @param v 
+  */
+void DisplayVec::printVector(const vector<int> v){
+for(int i = 0; i < v.size(); i++){
+    cout << v[i]<< endl;
+}
+}
+
+/**
+ * @brief returns the memory size of vec 
+ * 
+ * @param v 
+ * @return int 
+ */
+int DisplayVec::getVectMemSize(const vector<int> v){
+    int mem_size = 0; 
+    for(int i = 0; i < v.size(); i++){
+        mem_size += size(v[i]); //mem_size = mem_size + sizeof(v[i])
+    }
+    return mem_size;
+}
+DisplayVec::printMetaVector(const vector<int> v){
+
+}
+const int * DisplayVec::getAddress(vector<int> v, int index){
+
+}
